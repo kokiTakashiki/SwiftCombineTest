22308f0b1f912c42ef0cf488e98120a3f6f9850b
koki T <bluewhitered123@gmail.com>
2021-11-24 14:25:06 +0900

    stert project

---
+++ CombineTest/CombineTest/CombineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestApp.swift
+//  CombineTest
+//
+//  Created by 武田孝騎 on 2021/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
