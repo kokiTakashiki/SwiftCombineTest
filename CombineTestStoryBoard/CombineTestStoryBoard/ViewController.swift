b3e295043f5e2b5c0382dc0a61d89aa30578afd1
koki T <bluewhitered123@gmail.com>
2021-11-24 14:26:53 +0900

    start project combine storyboard var

---
+++ CombineTestStoryBoard/CombineTestStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CombineTestStoryBoard
+//
+//  Created by 武田孝騎 on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
