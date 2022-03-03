0cf4ac2cb433d631c45086b365af5df065f01b49
kokiTakeda <bluewhitered123@gmail.com>
2022-03-03 21:13:23 +0900

    github api ok

---
+++ CombineGithubMyList/CombineGithubMyListUITests/CombineGithubMyListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CombineGithubMyListUITestsLaunchTests.swift
+//  CombineGithubMyListUITests
+//
+//  Created by 武田孝騎 on 2022/03/02.
+//
+
+import XCTest
+
+class CombineGithubMyListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
